cb0e01b62f66fcf94e3892e501b336741b41a819
Serhii <777sirj777@gmail.com>
2020-01-19 21:31:57 +0200

    navController and button

---
+++ OSTableViewNavigation/OSTableViewController.h
@@ -0,0 +1,21 @@
+//
+//  OSTableViewController.h
+//  OSTableViewNavigation
+//
+//  Created by Sergii Onopriienko on 19.01.2020.
+//  Copyright © 2020 Onopriienko Sergii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OSTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSString *path;
+
+-(id) initWithFolderPath:(NSString *) path;
+
+@end
+
+NS_ASSUME_NONNULL_END
